13c644ee7dec674cf105630cab3e3f0507daf91f
Joseth222 <107093649+joseth22@users.noreply.github.com>
2023-03-14 10:47:38 -0600

    Prueba

---
+++ Proyecto_desarrollo/src/main/java/com/Proyecto_desarrollo/ProyectoDesarrolloApplication.java
@@ -0,0 +1,13 @@
+package com.Proyecto_desarrollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoDesarrolloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoDesarrolloApplication.class, args);
+	}
+
+}
---
+++ Proyecto_desarrollo/src/test/java/com/Proyecto_desarrollo/ProyectoDesarrolloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Proyecto_desarrollo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoDesarrolloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
