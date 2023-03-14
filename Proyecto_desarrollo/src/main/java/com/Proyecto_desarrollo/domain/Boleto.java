346a56620f7bc07e430ac4760931043229aabc88
andresguty29 <andgutierrez29@gmail.com>
2023-03-14 14:43:32 -0600

    Clases cliente, boleteria. Adelanto Index

---
+++ Proyecto_desarrollo/src/main/java/com/Proyecto_desarrollo/domain/Boleto.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.Proyecto_desarrollo.domain;
+
+/**
+ *
+ * @author Andrès
+ */
+public class Boleto {
+    
+    private static final long serialVersionUID = 1L;
+    
+    
+    
+}
